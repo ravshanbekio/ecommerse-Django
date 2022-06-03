fdc146a7676aefe4bfdece42a3302214d73e2178
TheRavshanDev <88030420+TheRavshanDev@users.noreply.github.com>
2022-06-03 21:21:32 +0500

    Added barcode for products

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Category, Category2, Tovar, Wishlist
+from .models import Category, Category2, Tovar, Wishlist, Barcode
 
 admin.site.register(Category)
 admin.site.register(Category2)
 admin.site.register(Tovar)
 admin.site.register(Wishlist)
+admin.site.register(Barcode)
--- main/models.py
+++ main/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from base_user.models import Main_User
+import barcode
+from barcode.writer import ImageWriter
+from io import BytesIO
+from django.core.files import File
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -32,6 +36,24 @@ class Tovar(models.Model):
 
     def __str__(self):
         return self.name
+
+class Barcode(models.Model):
+    product = models.ForeignKey(Tovar, on_delete=models.SET_NULL, null=True)
+    country_id = models.CharField(max_length=1)
+    manufacturer_id = models.CharField(max_length=6)
+    product_number = models.CharField(max_length=5)
+    barcode = models.ImageField(upload_to='barcode/', blank=True)
+
+    def __str__(self):
+        return self.product.name
+
+    def save(self, *args, **kwargs):
+        EAN = barcode.get_barcode_class('ean13')
+        ean = EAN(f"{self.country_id}{self.manufacturer_id}{self.product_number}", writer=ImageWriter())
+        buffer = BytesIO()
+        ean.write(buffer)
+        self.barcode.save(f"{self.product.name}.png",File(buffer), save=False)
+        return super().save(*args, **kwargs)
 
 class Rate(models.Model):
     rate = models.PositiveSmallIntegerField()
